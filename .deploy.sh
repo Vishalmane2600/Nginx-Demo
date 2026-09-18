a870bb0794ff27b88060208e991258a85195b87f
Vishal Mane <95902011+Vishalmane2600@users.noreply.github.com>
2026-09-18 19:51:56 +0530

    Update .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -36,4 +36,4 @@ echo "✅ Deployment completed successfully!"
 echo "=================================="
 
 echo ""
-echo "Application URL:http://http://54.243.17.232"
+echo "Application URL: http://54.243.17.232/"
